a917f52b8956564391bdc535ea4600204c233086
David Callay <david.callay@valtech.com>
2022-05-29 19:49:56 +0200

    Initial

---
+++ DataAccessLibrary/SampleDataAccess.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.Extensions.Caching.Memory;
+
+namespace DataAccessLibrary
+{
+    public class SampleDataAccess
+    {
+        private readonly IMemoryCache memoryCache;
+
+        public SampleDataAccess(IMemoryCache memoryCache)
+        {
+            this.memoryCache = memoryCache;
+        }
+
+        public List<EmployeeModel> GetEmployees()
+        {
+            List<EmployeeModel> employees = new()
+            {
+                new() { FirstName = "David", LastName = "Callay" },
+                new() { FirstName = "John", LastName = "Doe" },
+                new() { FirstName = "Sue", LastName = "Cucumber" }
+            };
+
+            var etst = string.Empty;
+            // comment added
+
+            Thread.Sleep(3000);
+
+            return employees;
+        }
+
+        public async Task<List<EmployeeModel>> GetEmployeesAsync()
+        {
+            List<EmployeeModel> employees = new()
+            {
+                new() { FirstName = "David", LastName = "Callay" },
+                new() { FirstName = "John", LastName = "Doe" },
+                new() { FirstName = "Sue", LastName = "Cucumber" }
+            };
+
+            await Task.Delay(3000);
+
+            return employees;
+        }
+
+        public async Task<List<EmployeeModel>> GetEmployeesCache()
+        {
+            List<EmployeeModel> employees;
+
+            employees = memoryCache.Get<List<EmployeeModel>>("employees");
+
+            if(employees is null)
+            {
+                employees = new()
+                {
+                    new() { FirstName = "David", LastName = "Callay" },
+                    new() { FirstName = "John", LastName = "Doe" },
+                    new() { FirstName = "Sue", LastName = "Cucumber" }
+                };
+
+                await Task.Delay(3000);
+
+                memoryCache.Set("employees", employees, TimeSpan.FromMinutes(1));
+            }
+
+            return employees;
+        }
+    }
+}
---
+++ InMemoryCache/Program.cs
@@ -0,0 +1,37 @@
+using DataAccessLibrary;
+using InMemoryCache.Data;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
+// Use interfaces for DI instead
+builder.Services.AddTransient<SampleDataAccess>();
+// Singleton (across all different instances
+// protect data if it is not meant to be used for all threads
+builder.Services.AddMemoryCache();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
